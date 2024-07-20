4bb2e6be62b85a8e82c3aa18b85b849bf0aa5a03
ychang74 <y.chang20@imperial.ac.uk>
2024-07-20 22:20:18 +0800

    Create waveunet.py

---
+++ audio_models/waveunet/waveunet.py
@@ -0,0 +1,364 @@
+import argparse
+import os
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from crop import centre_crop
+from resample import Resample1d
+from conv import ConvLayer
+
+class UpsamplingBlock(nn.Module):
+    def __init__(self, n_inputs, n_shortcut, n_outputs, kernel_size, stride, depth, conv_type, res):
+        super(UpsamplingBlock, self).__init__()
+        assert(stride > 1)
+
+        # CONV 1 for UPSAMPLING
+        if res == "fixed":
+            self.upconv = Resample1d(n_inputs, 15, stride, transpose=True)
+        else:
+            self.upconv = ConvLayer(n_inputs, n_inputs, kernel_size, stride, conv_type, transpose=True)
+
+        self.pre_shortcut_convs = nn.ModuleList([ConvLayer(n_inputs, n_outputs, kernel_size, 1, conv_type)] +
+                                                [ConvLayer(n_outputs, n_outputs, kernel_size, 1, conv_type) for _ in range(depth - 1)])
+
+        # CONVS to combine high- with low-level information (from shortcut)
+        self.post_shortcut_convs = nn.ModuleList([ConvLayer(n_outputs + n_shortcut, n_outputs, kernel_size, 1, conv_type)] +
+                                                 [ConvLayer(n_outputs, n_outputs, kernel_size, 1, conv_type) for _ in range(depth - 1)])
+
+    def forward(self, x, shortcut):
+        # UPSAMPLE HIGH-LEVEL FEATURES
+        upsampled = self.upconv(x)
+
+        for conv in self.pre_shortcut_convs:
+            upsampled = conv(upsampled)
+
+        # Prepare shortcut connection
+        combined = centre_crop(shortcut, upsampled)
+
+        # Combine high- and low-level features
+        for conv in self.post_shortcut_convs:
+            combined = conv(torch.cat([combined, centre_crop(upsampled, combined)], dim=1))
+        return combined
+
+    def get_output_size(self, input_size):
+        curr_size = self.upconv.get_output_size(input_size)
+
+        # Upsampling convs
+        for conv in self.pre_shortcut_convs:
+            curr_size = conv.get_output_size(curr_size)
+
+        # Combine convolutions
+        for conv in self.post_shortcut_convs:
+            curr_size = conv.get_output_size(curr_size)
+
+        return curr_size
+
+class DownsamplingBlock(nn.Module):
+    def __init__(self, n_inputs, n_shortcut, n_outputs, kernel_size, stride, depth, conv_type, res):
+        super(DownsamplingBlock, self).__init__()
+        assert(stride > 1)
+
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+        # CONV 1
+        self.pre_shortcut_convs = nn.ModuleList([ConvLayer(n_inputs, n_shortcut, kernel_size, 1, conv_type)] +
+                                                [ConvLayer(n_shortcut, n_shortcut, kernel_size, 1, conv_type) for _ in range(depth - 1)])
+
+        self.post_shortcut_convs = nn.ModuleList([ConvLayer(n_shortcut, n_outputs, kernel_size, 1, conv_type)] +
+                                                 [ConvLayer(n_outputs, n_outputs, kernel_size, 1, conv_type) for _ in
+                                                  range(depth - 1)])
+
+        # CONV 2 with decimation
+        if res == "fixed":
+            self.downconv = Resample1d(n_outputs, 15, stride) # Resampling with fixed-size sinc lowpass filter
+        else:
+            self.downconv = ConvLayer(n_outputs, n_outputs, kernel_size, stride, conv_type)
+
+    def forward(self, x):
+        # PREPARING SHORTCUT FEATURES
+        shortcut = x
+        for conv in self.pre_shortcut_convs:
+            shortcut = conv(shortcut)
+
+        # PREPARING FOR DOWNSAMPLING
+        out = shortcut
+        for conv in self.post_shortcut_convs:
+            out = conv(out)
+
+        # DOWNSAMPLING
+        out = self.downconv(out)
+
+        return out, shortcut
+
+    def get_input_size(self, output_size):
+        curr_size = self.downconv.get_input_size(output_size)
+
+        for conv in reversed(self.post_shortcut_convs):
+            curr_size = conv.get_input_size(curr_size)
+
+        for conv in reversed(self.pre_shortcut_convs):
+            curr_size = conv.get_input_size(curr_size)
+        return curr_size
+
+class Waveunet(nn.Module):
+    def __init__(self, num_inputs, num_channels, num_outputs, instruments, kernel_size, target_output_size, conv_type, res, separate=False, depth=1, strides=2, freeze_dec1=False):
+        super(Waveunet, self).__init__()
+
+        self.num_levels = len(num_channels)
+        self.strides = strides
+        self.kernel_size = kernel_size
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+        self.depth = depth
+        self.instruments = instruments
+        self.separate = separate
+        self.freeze_dec1 = freeze_dec1
+
+        # Only odd filter kernels allowed
+        assert(kernel_size % 2 == 1)
+
+        self.waveunets = nn.ModuleDict()
+
+        model_list = instruments if separate else ["ALL"]
+        # Create a model for each source if we separate sources separately, otherwise only one (model_list=["ALL"])
+        for instrument in model_list:
+            module = nn.Module()
+
+            module.downsampling_blocks = nn.ModuleList()
+            module.upsampling_blocks = nn.ModuleList()
+            module.upsampling_blocks2 = nn.ModuleList()
+            for i in range(self.num_levels - 1):
+                in_ch = num_inputs if i == 0 else num_channels[i]
+
+                module.downsampling_blocks.append(
+                    DownsamplingBlock(in_ch, num_channels[i], num_channels[i+1], kernel_size, strides, depth, conv_type, res))
+
+            for i in range(0, self.num_levels - 1):
+                module.upsampling_blocks.append(
+                    UpsamplingBlock(num_channels[-1-i], num_channels[-2-i], num_channels[-2-i], kernel_size, strides, depth, conv_type, res))
+                module.upsampling_blocks2.append(
+                    UpsamplingBlock(num_channels[-1-i], num_channels[-2-i], num_channels[-2-i], kernel_size, strides, depth, conv_type, res))
+
+            module.bottlenecks = nn.ModuleList(
+                [ConvLayer(num_channels[-1], num_channels[-1], kernel_size, 1, conv_type) for _ in range(depth)])
+
+            module.bottlenecks2 = nn.ModuleList(
+                [ConvLayer(num_channels[-1], num_channels[-1], kernel_size, 1, conv_type) for _ in range(depth)])
+
+            # Output conv
+            outputs = num_outputs * len(model_list)
+            module.output_conv = nn.Conv1d(num_channels[0], outputs, 1)
+            module.output_conv2 = nn.Conv1d(num_channels[0], outputs, 1)
+
+            if self.freeze_dec1:
+                for param in module.bottlenecks.parameters():
+                    param.requires_grad = False
+                for param in module.output_conv.parameters():
+                    param.requires_grad = False
+                for param in module.upsampling_blocks.parameters():
+                    param.requires_grad = False
+            self.waveunets[instrument] = module
+
+        self.set_output_size(target_output_size)
+
+    def set_output_size(self, target_output_size):
+        self.target_output_size = target_output_size
+
+        self.input_size, self.output_size = self.check_padding(target_output_size)
+        print("Using valid convolutions with " + str(self.input_size) + " inputs and " + str(self.output_size) + " outputs")
+
+        assert((self.input_size - self.output_size) % 2 == 0)
+        self.shapes = {"output_start_frame" : (self.input_size - self.output_size) // 2,
+                       "output_end_frame" : (self.input_size - self.output_size) // 2 + self.output_size,
+                       "output_frames" : self.output_size,
+                       "input_frames" : self.input_size}
+
+        print(self.shapes)
+
+    def check_padding(self, target_output_size):
+        # Ensure number of outputs covers a whole number of cycles so each output in the cycle is weighted equally during training
+        bottleneck = 1
+
+        while True:
+            out = self.check_padding_for_bottleneck(bottleneck, target_output_size)
+            if out is not False:
+                return out
+            bottleneck += 1
+
+    def check_padding_for_bottleneck(self, bottleneck, target_output_size):
+        module = self.waveunets[[k for k in self.waveunets.keys()][0]]
+        try:
+            curr_size = bottleneck
+            for idx, block in enumerate(module.upsampling_blocks):
+                curr_size = block.get_output_size(curr_size)
+            output_size = curr_size
+
+            # Bottleneck-Conv
+            curr_size = bottleneck
+            for block in reversed(module.bottlenecks):
+                curr_size = block.get_input_size(curr_size)
+            for idx, block in enumerate(reversed(module.downsampling_blocks)):
+                curr_size = block.get_input_size(curr_size)
+
+            assert(output_size >= target_output_size)
+            return curr_size, output_size
+        except AssertionError as e:
+            return False
+
+    def forward_module(self, x, module, eps):
+        '''
+        A forward pass through a single Wave-U-Net (multiple Wave-U-Nets might be used, one for each source)
+        :param x: Input mix
+        :param module: Network module to be used for prediction
+        :return: Source estimates
+        '''
+        shortcuts = []
+        out = x
+
+        # DOWNSAMPLING BLOCKS
+        for block in module.downsampling_blocks:
+            out, short = block(out)
+            shortcuts.append(short)
+
+        code = out
+        # BOTTLENECK CONVOLUTION
+        for conv in module.bottlenecks:
+            out = conv(out)
+
+        for conv in module.bottlenecks2:
+            out_inf = conv(code)
+
+        # UPSAMPLING BLOCKS
+        for idx, block in enumerate(module.upsampling_blocks):
+            out = block(out, shortcuts[-1 - idx])
+
+        for idx, block in enumerate(module.upsampling_blocks2):
+            out_inf = block(out_inf, shortcuts[-1 - idx])
+
+        # OUTPUT CONV
+        # location map: out is in [0, 1]
+        out = module.output_conv(out)
+        out = (torch.tanh(out) + 1) / 2
+        # perturbation magnititude: out_inf [-eps, eps]
+        out_inf = module.output_conv2(out_inf)
+        out_inf = eps * torch.tanh(out_inf)
+
+        if not self.training:  # At test time clip predictions to valid amplitude range
+            # out = out.clamp(min=-1.0, max=1.0)
+            out_0 = torch.where(out< 0.5, torch.zeros_like(out).detach(), torch.ones_like(out).detach())
+        elif self.training:
+            out_0 = torch.where(torch.rand(out.shape).cuda()<0.5, out, torch.where(out<0.5, torch.zeros_like(out), torch.ones_like(out)).detach())
+
+        # simply cut
+        out = out[:,:,:self.target_output_size]
+        out_inf = out_inf[:,:,:self.target_output_size]
+        out_0 = out_0[:,:,:self.target_output_size]
+        x = x[:,:,:self.target_output_size]
+
+        if self.freeze_dec1:
+            out_x = out_inf + x
+        else:
+            out_x = out_inf * out_0 + x
+        return out_x, out_inf, out_0, out
+
+    def forward(self, x, eps, inst=None):
+        # Add channel number
+        batch_size, seq_length = x.shape
+        x = x.view(batch_size, 1, seq_length)
+
+
+        curr_input_size = x.shape[-1]
+
+        if curr_input_size != self.input_size:
+            pad_left = (self.input_size - self.output_size) // 2
+            pad_right =  (self.input_size - self.output_size) // 2 + self.output_size - self.target_output_size
+            # x = F.pad(x, (pad_left, pad_right), mode='reflect')
+            pad_func = nn.ReflectionPad1d((pad_left, pad_right))
+            x = pad_func(x)
+
+        curr_input_size = x.shape[-1]
+        assert(curr_input_size == self.input_size) # User promises to feed the proper input himself, to get the pre-calculated (NOT the originally desired) output size
+
+        if self.separate:
+            return {inst : self.forward_module(x, self.waveunets[inst], eps)}
+        else:
+            assert(len(self.waveunets) == 1)
+            out_x, out_inf, out_0, out = self.forward_module(x, self.waveunets["ALL"], eps)
+            return out_x, out_inf, out_0, out
+            '''
+            out_dict = {}
+            for idx, inst in enumerate(self.instruments):
+                out_dict[inst] = out[:, idx * self.num_outputs:(idx + 1) * self.num_outputs]
+            return out_dict
+            '''
+
+
+
+
+# self-test
+def test():
+    x = torch.randn(16, 84800)
+
+    num_features = [args.features*i for i in range(1, args.levels+1)] if args.feature_growth == "add" else \
+                   [args.features*2**i for i in range(0, args.levels)]
+    target_outputs = int(5.3 * args.sr)
+    model = Waveunet(args.channels, num_features, args.channels, args.instruments, kernel_size=args.kernel_size,
+                     target_output_size=target_outputs, depth=args.depth, strides=args.strides,
+                     conv_type=args.conv_type, res=args.res, separate=args.separate)
+
+    if args.cuda:
+        x = x.cuda()
+        model.cuda()
+
+    model.train()
+    x_out, _, _, _ = model(x, eps=0.05)
+
+    print('Generator parameters:', sum(p.numel() for p in model.parameters() if p.requires_grad))
+    print(x_out.shape)
+    # assert x_out.shape == x.shape, (x.shape, x_out.shape)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--instruments', type=str, nargs='+', default=["bass", "drums", "other", "vocals"],
+                        help="List of instruments to separate (default: \"bass drums other vocals\")")
+    parser.add_argument('--cuda', action='store_true',
+                        help='Use CUDA (default: False)')
+    parser.add_argument('--num_workers', type=int, default=1,
+                        help='Number of data loader worker threads (default: 1)')
+    parser.add_argument('--features', type=int, default=32,
+                        help='Number of feature channels per layer')
+    parser.add_argument('--cycles', type=int, default=2,
+                        help='Number of LR cycles per epoch')
+    parser.add_argument('--levels', type=int, default=6,
+                        help="Number of DS/US blocks")
+    parser.add_argument('--depth', type=int, default=1,
+                        help="Number of convs per block")
+    parser.add_argument('--sr', type=int, default=16000,
+                        help="Sampling rate")
+    parser.add_argument('--channels', type=int, default=1,
+                        help="Number of input audio channels")
+    parser.add_argument('--kernel_size', type=int, default=5,
+                        help="Filter width of kernels. Has to be an odd number")
+    parser.add_argument('--output_size', type=float, default=6.0,
+                        help="Output duration")
+    parser.add_argument('--strides', type=int, default=4,
+                        help="Strides in Waveunet")
+    parser.add_argument('--conv_type', type=str, default="gn",
+                        help="Type of convolution (normal, BN-normalised, GN-normalised): normal/bn/gn")
+    parser.add_argument('--res', type=str, default="learned",
+                        help="Resampling strategy: fixed sinc-based lowpass filtering or learned conv layer: fixed/learned")
+    parser.add_argument('--separate', type=int, default=0,
+                        help="Train separate model for each source (1) or only one (0)")
+    parser.add_argument('--feature_growth', type=str, default="double",
+                        help="How the features in each layer should grow, either (add) the initial number of features each time, or multiply by 2 (double)")
+
+    args = parser.parse_args()
+
+
+    test()
